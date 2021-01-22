a058496cc30aa81c04aa650cb0c26f9809ffec42
wangjq <jqwang2009@163.com>
2021-01-22 17:05:41 +0800

    MainWindow: add MainWindow

---
+++ Tongyu.First.Retail/Tongyu.First.Utils/Helpers/MathHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tongyu.First.Utils
+{
+    public class MathHelper
+    {
+        public static bool IsEqual(double first,double second,double tolerance = 1e-5)
+        {
+            if (double.IsNaN(first) || double.IsNaN(second))
+                return false;
+            return Math.Abs(first - second) < tolerance;
+        }
+    }
+}
